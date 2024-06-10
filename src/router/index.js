d96e01aefbfde39c544d87940e71fe9638f496cf
nhatha <nhathmps07871@fpt.edu.vn>
2024-06-10 22:20:47 +0700

    add pkg new init

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,33 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import auth from '../middleware/auth'
+import { useAuthStore } from '../stores/AuthStore'
+
+const routeMiddleware = (context, middlewares, index) => {
+  const nextMiddleware = middlewares[index]
+
+  if (!nextMiddleware) {
+    return context.next
+  }
+
+  return () => {
+    nextMiddleware({
+      ...context,
+      next: routeMiddleware(context, middlewares, index + 1)
+    })
+  }
+}
+
+const ifNotAuthenticated = (to, from, next) => {
+  if (localStorage.getItem('user-token')) {
+    next({ name: 'DashBoard' })
+  } else {
+    if (to?.path == '/') {
+      next({ name: 'Login' })
+    }
+    next()
+  }
+}
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
