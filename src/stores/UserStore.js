e9bc27a18392ff740d39fdc88e9511eefd455c9a
Truong-2309 <truonghmps23087@fpt.edu.vn>
2024-06-10 22:23:25 +0700

    add save user
    Signed-off-by: nhatha <nhathmps07871@fpt.edu.vn>

---
+++ src/stores/UserStore.js
@@ -0,0 +1,47 @@
+import { defineStore } from "pinia";
+import UserService from "../services/UserService";
+
+export const useUserStore = defineStore("user", {
+    state: () => ({
+        user: {},
+    }),
+    getters: {},
+    actions: {
+        async listUser(request) {
+            return new Promise((resolve, reject) => {
+                UserService.listUser(request)
+                    .then(({ data }) => {
+                        resolve(data);
+                    })
+                    .catch(({ response }) => reject(response));
+            });
+        },
+        async getUserTicketOpenDates(request) {
+            return new Promise((resolve, reject) => {
+                UserService.getUserTicketOpenDates(request)
+                    .then(({ data }) => {
+                        resolve(data);
+                    })
+                    .catch(({ response }) => reject(response));
+            });
+        },
+        async editUser(payload, id) {
+            return new Promise((resolve, reject) => {
+                UserService.editUser(payload, id)
+                    .then(({ data }) => {
+                        resolve(data);
+                    })
+                    .catch(({ response }) => reject(response));
+            });
+        },
+        async deleteUser(id) {
+            return new Promise((resolve, reject) => {
+                UserService.deleteUser(id)
+                    .then(({ data }) => {
+                        resolve(data);
+                    })
+                    .catch(({ response }) => reject(response));
+            });
+        },
+    },
+});
