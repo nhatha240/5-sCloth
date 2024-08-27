95c1a87ba7bae66378dbadd78baa2260d91fc76f
nlvu <107091980+nlvu@users.noreply.github.com>
2024-08-27 13:47:10 +0700

    Update common.js

--- src/constant/common.js
+++ src/constant/common.js
@@ -4,7 +4,11 @@ export const CATEGORY = {
     'childShirt': 'Đồ Trẻ Em',
     'accessory': 'Phụ Kiện',
 }
-
+export const ORDER_STATUS = {
+    'ready': 'Ready',
+    'shipped': 'Shipped',
+    'received': 'Received',
+}
 export const NAME_LINK_ROUTER = {
     'Books': 'ProductListView',
 }
