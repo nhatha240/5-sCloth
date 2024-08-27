0e8813957222c43447f3743ffb41f342583b11ea
nlvu <107091980+nlvu@users.noreply.github.com>
2024-08-27 13:42:52 +0700

    Update ConfirmDeleteItem.vue

--- src/components/ConfirmDeleteItem.vue
+++ src/components/ConfirmDeleteItem.vue
@@ -1,23 +1,5 @@
 <template>
-    <VueFinalModal v-model="showModal" class="category-modal" content-class="category-modal-content"
-        overlay-transition="vfm-fade" content-transition="vfm-fade" :focusTrap="false">
-        <div class="">
-            <div class="title-modal pb-[20px]">
-                Delete Items
-            </div>
-            <div class="content-text pb-[20px]" v-html="contentText">
-            </div>
-            <img class="cursor-pointer absolute right-3 top-3" src="/images/close_modal_category_icon.svg" alt="" @click="cancelModal">
-            <div class="flex items-center justify-end gap-[28px]">
-                <button type="button" class="font-common text-[#F0142F] text-base bg-[#FFFFFF] border-none py-2 px-[25px] rounded-[4px]" @click="cancelModal">
-                    Cancel
-                </button>
-                <button type="submit" class="font-common text-[#FFFFFF] text-base bg-[#F0142F] py-2 px-[20px] rounded-[4px] flex items-center" @click="deleteItem">
-                    Delete
-                </button>
-            </div>
-        </div>
-    </VueFinalModal>
+   
 </template>
 
 <script lang="js" setup>
